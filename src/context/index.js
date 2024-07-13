84666c2029065d6b5187262b9ff490d624754f49
Nisha0202 <nishajabatunnessa@gmail.com>
2024-07-14 00:04:48 +0600

    adnan

--- src/context/index.js
+++ src/context/index.js
@@ -11,7 +11,9 @@ export const AppWrapper = ({ children }) => {
   const fetchCurrentUser = useCallback(async () => {
     setLoading(true);
     try {
+      console.log("adnan1")
       const response = await axios.get('/api/currentprofile');
+      console.log("adnan2")
       setUser(response.data.data);
       console.log(response.data.data);
     } catch (error) {
